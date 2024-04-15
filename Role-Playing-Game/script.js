ec8ba58ac29f63f4c2672eb83a54157eb59ea913
CodeCruser <elouardichifahd1998@gmail.com>
2024-04-15 23:17:35 +0100

    step 35

--- Role-Playing-Game/script.js
+++ Role-Playing-Game/script.js
@@ -17,3 +17,7 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 
 const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goStore () {
+
+};
